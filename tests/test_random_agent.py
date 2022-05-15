c963a95ac6314db6e7bcf372f5d99cc5cfb05b29
hhn1n15 <hainhbkhn@gmail.com>
2022-05-14 20:13:45 -0400

    bring back lunar lander

--- tests/test_random_agent.py
+++ tests/test_random_agent.py
@@ -2,19 +2,27 @@ from pdomains import *
 import gym
 import numpy as np
 
-env=gym.make('pdomains-mg-memory-s17-v0', rendering=False)
+# env=gym.make('pdomains-mg-memory-s17-v0', rendering=False)
+# env=gym.make('pdomains-car-flag-continuous-v0', rendering=False)
+env=gym.make('pdomains-half-cheetah-v-v0', rendering=False)
 env.reset()
 
 num_eps = 100
 ep_cnt = 0
 success = []
+rewards = []
+ep_reward = 0
 
 while (ep_cnt < num_eps):
     action = env.action_space.sample()
-    _, _, done, info = env.step(action)
+    _, reward, done, info = env.step(action)
+    ep_reward += reward
     if done:
         ep_cnt += 1
+        rewards.append(ep_reward)
+        ep_reward = 0
         success.append(info["success"])
         env.reset()
 
-print(np.mean(success), np.std(success))
+print("Success rate:", np.mean(success), np.std(success))
+print("Reward:", np.mean(rewards), np.std(rewards))
