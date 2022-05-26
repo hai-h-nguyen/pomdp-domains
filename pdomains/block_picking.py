a190b404631f0409cbfec277520b17d68be9affe
hhn1n15 <hainhbkhn@gmail.com>
2022-05-26 10:01:23 -0400

    minor

--- pdomains/block_picking.py
+++ pdomains/block_picking.py
@@ -10,20 +10,23 @@ import matplotlib.pyplot as plt
 import time
 
 class BlockEnv(gym.Env):
-    def __init__(self, seed=0, rendering=False):
+    def __init__(self, seed=0, rendering=False, robot='kuka', action_sequence='pxyz'):
 
         workspace = np.asarray([[0.3, 0.7],
                                 [-0.2, 0.2],
                                 [0.01, 0.25]])
 
         self.env_config = {'workspace': workspace, 'max_steps': 100, 'obs_size': 84, 'render': False, 'fast_mode': True,
-                        'seed': 0, 'action_sequence': 'pxyz', 'num_objects': 1, 'random_orientation': False,
-                        'reward_type': 'sparse', 'simulate_grasp': True, 'perfect_grasp': False, 'robot': 'ur5_robotiq',
-                        'workspace_check': 'point', 'physics_mode': 'fast', 'hard_reset_freq': 1000,
+                        'seed': 0, 'action_sequence': action_sequence, 'num_objects': 1, 'random_orientation': False,
+                        'reward_type': 'sparse', 'simulate_grasp': True, 'perfect_grasp': False, 'robot': robot,
+                        'workspace_check': 'point', 'physics_mode': 'fast', 'hard_reset_freq': 1000, 'view_scale': 1.0,
                         'object_scale_range': (1, 1), 'obs_type': 'pixel',
-                        'view_type': 'camera_fix_height'}
+                        'view_type': 'camera_center_z_height'}
 
-        self.planner_config = {'random_orientation': False, 'dpos': 0.05, 'drot': np.pi/8}
+        self.planner_config = {'random_orientation': True, 'dpos': 0.05, 'drot': np.pi/8}
+
+        self.xyz_range = self.planner_config['dpos']
+        self.r_range = self.planner_config['drot']
 
         self.env_config['render'] = rendering
         self.seed(seed)
@@ -35,11 +38,12 @@ class BlockEnv(gym.Env):
         self.show = rendering
         self.obs = None
 
-        high_action = np.ones(4)
+        self.action_dim = len(self.env_config['action_sequence'])
+        high_action = np.ones(self.action_dim)
         self.action_space = spaces.Box(-high_action, high_action)
 
         low = np.zeros((84, 84, 3))
-        high = 255*np.ones((84, 84, 3))
+        high = np.ones((84, 84, 3))
         self.observation_space = spaces.Box(low=low, high=high, shape=(84, 84, 3), dtype=np.float32)
 
         self.target_obj_idx = 0
@@ -49,8 +53,13 @@ class BlockEnv(gym.Env):
 
     def query_expert(self):
         action = self.core_env.getNextAction(self.target_obj_idx)
-        action[1:] /= 0.05
-        action[0] = 2*action[0] - 1
+        action[1:4] /= self.xyz_range
+
+        if self.action_dim == 5:
+            action[4] /= self.r_range
+
+        if self.env_config['robot'] == 'kuka':
+            action[0] = 2*action[0] - 1
 
         return action
 
@@ -67,13 +76,17 @@ class BlockEnv(gym.Env):
         return np.transpose(stacked, (2, 1, 0))
 
     def step(self, action):
-        action[1:] *= 0.05  # scale from [-1, 1] to [-0.05, 0.05] for xyz
-        action[0] = 0.5 * (action[0] + 1)  # [-1, 1] to [0, 1] for p
-        # action = np.zeros_like(action)
+        action[1:4] *= self.xyz_range  # scale from [-1, 1] to [-0.05, 0.05] for xyz
+
+        if self.action_dim == 5:  # scale from [-1, 1] to [-np.pi/8, np.pi/8]
+            action[4] *= self.r_range
+
+        if self.env_config['robot'] == 'kuka':
+            action[0] = 0.5 * (action[0] + 1)  # [-1, 1] to [0, 1] for p
         (state, _, obs), reward, done = self.core_env.step(action)
 
-        plt.imshow(obs[0], vmin=0, vmax=0.2)
-        plt.show()
+        # plt.imshow(obs[0], vmin=0, vmax=0.2)
+        # plt.show()
 
         self.obs = self._process_obs(state, obs, reward)
 
