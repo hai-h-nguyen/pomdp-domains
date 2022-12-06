5ee75034c8dbd422033b5136ebe825f3e510de11
hhn1n15 <hainhbkhn@gmail.com>
2022-12-06 08:54:38 -0500

    add a domain for slac

--- pdomains/__init__.py
+++ pdomains/__init__.py
@@ -93,6 +93,12 @@ register(
 )
 
 register(
+    id='pdomains-block-picking-64-v0',
+    entry_point='pdomains.block_picking_64:BlockEnv',
+    max_episode_steps=50,
+)
+
+register(
     id='pdomains-block-pulling-v0',
     entry_point='pdomains.block_pulling:BlockEnv',
     max_episode_steps=50,
